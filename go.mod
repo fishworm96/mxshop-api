e868a0de30cfb0f938285a91ff6ca6e73548a5ea
fishworm96 <245525860@qq.com>
2022-06-20 14:44:51 +0800

    添加jwt认真

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require google.golang.org/grpc v1.47.0
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
--- user-web/api/user.go
+++ user-web/api/user.go
@@ -6,12 +6,15 @@ import (
 	"mxshop-api/user-web/forms"
 	"mxshop-api/user-web/global"
 	"mxshop-api/user-web/global/response"
+	"mxshop-api/user-web/middlewares"
+	"mxshop-api/user-web/models"
 	"mxshop-api/user-web/proto"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.uber.org/zap"
@@ -162,8 +165,31 @@ func PassWordLogin(c *gin.Context) {
 			})
 		} else {
 			if passRsp.Success {
-				c.JSON(http.StatusOK, map[string]string{
-					"msg": "登录成功",
+				// 生成token
+				j := middlewares.NewJWT()
+				claims := models.CustomClaims{
+					ID: uint(rsp.Id),
+					NickName: rsp.NickName,
+					AuthorityId: uint(rsp.Role),
+					StandardClaims: jwt.StandardClaims{
+						NotBefore: time.Now().Unix(), // 签名时间
+						ExpiresAt: time.Now().Unix() + 60*60*24*30, // 30天过期
+						Issuer: "imooc",
+					},
+				}
+				token, err := j.CreateToken(claims)
+				if err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{
+						"msg": "生成token失败",
+					})
+					return
+				}
+
+				c.JSON(http.StatusOK, gin.H{
+					"id": rsp.Id,
+					"nick_name": rsp.NickName,
+					"token": token,
+					"expired_at": (time.Now().Unix() + 60*60*24*30)*1000,
 				})
 			} else {
 				c.JSON(http.StatusBadRequest, map[string]string{
